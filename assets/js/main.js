5e15b4fce3f53a6075e690e9f1ab5bf09d0ce4a9
babadav <babadav@gmail.com>
2017-09-12 18:51:49 -0400

    first

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+console.log("script.js");
+
+
+
+$('.indica-description').on('click', function(){
+	$('.indica-description').toggleClass('.active');
+	$('.indica-hidden-text').toggleClass('.active');
+
+});
+
+
+
+
+$('.sativa-description').on('click', function(){
+	TweenMax.to('.sativa-description', 1, {
+	top: "0%"
+	});
+
+});
