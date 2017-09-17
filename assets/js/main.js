54b082efd6567f0c6c2b272770d7c9682818c4a3
babadav <babadav@gmail.com>
2017-09-17 18:43:59 -0400

    almost done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,26 @@
 console.log("script.js");
 
+function indicaEvents() {
+	$('.indica-image').on('click', function(){
+	$('.indica-hidden-text').toggleClass('indica-active');
+	$('.indica-description').toggleClass('indica-description-active');
+
+	$('.white-arrow-down').toggleClass('indica-active-x');
+
+	});
+}
+
+function sativaEvents() {
+	$('.sativa-description').on('click', function(){
+	$('.sativa-hidden-text').toggleClass('sativa-active');
+	$('.sativa-description').toggleClass('sativa-description-active');
+
+	$('.white-arrow-down').toggleClass('sativa-active-x');
+
+	});
+}
 
 
-$('.indica-image').on('click', function(){
-	$('.indica-description').toggleClass('active');
-	$('.indica-hidden-text').toggleClass('active');
+indicaEvents();
+sativaEvents();
 
-});
-
-
-
-
-// $('.sativa-description').on('click', function(){
-// 	TweenMax.to('.sativa-description', 1, {
-// 	top: "0%"
-// 	});
-
-// });
